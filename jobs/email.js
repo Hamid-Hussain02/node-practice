8bbbe8c6f21d991401eb99edd99a11be9e15982a
Hamid Hussian <hamidhussian@MP15-HamidH-SE-363.local>
2022-02-22 11:02:24 +0500

    jobs|events

--- jobs/email.js
+++ jobs/email.js
@@ -1,7 +1,7 @@
 const mailer = require('nodemailer');
 const nodeCron = require('node-cron');
 
-// Creating a transporter
+
 const transporter = mailer.createTransport({
     service: 'gmail',
     auth: {
@@ -11,9 +11,7 @@ const transporter = mailer.createTransport({
 });
 
 const scheduleEmail = (req, res) => {
-    // cron.schedule('*/10 * * * *', sendEmail("Hi, this is a test email sent to you by corn job."));
-    // sendEmail('Hi, this is a test email sent to you by corn job.')
-    nodeCron.schedule("* * * * * *",function sendEmail(){
+    nodeCron.schedule("* 10 * * * *",function sendEmail(){
         //sending the email
         transporter.sendMail({
             from: '"Hamid" <iamhamid28@mailinator.com>',
